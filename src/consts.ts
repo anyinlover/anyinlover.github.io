b5045122ddf847f2da7a35b1233bd4bbbb468f2a
Anyinlover <guhangsong@outlook.com>
2024-05-05 10:34:52 +0800

    rebuild blog by astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '木公的小屋';
+export const SITE_DESCRIPTION = '欢迎来到木公的小屋，一起探究技术的无限可能！';
