23670af6e42a305eb578e07559337aa3d7cfe9f5
Anyinlover <guhangsong@outlook.com>
2024-05-05 12:34:14 +0800

    add math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://anyinlover.github.io/',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+	}
 });
