c666caaef38622b4a0abd83a4d9965a192820e06
Anyinlover <guhangsong@outlook.com>
2024-11-11 08:35:31 +0800

    convert to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'accent': '#2337ff',
+				'accent-dark': '#000d8a',
+			}
+		},
 	},
 	plugins: [],
 }
